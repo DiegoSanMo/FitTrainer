dd9e5d539bd42e8223f92c3e214e8e7d775768e1
Diego Mosan <diegomosan@Diegos-MBP.bbrouter>
2024-04-15 15:25:23 -0600

    Added new folder structure and new routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,56 @@ import "./App.css";
 
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
-import { Button, Typography } from "@mui/material";
-import { NavBar } from "./components/NavBar/NavBar.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomeLayout, Plans, Workout, Customers, Register, Login, Landing, Dashboard, Error } from "./pages/index.ts";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Landing/>
+      },
+        {
+          path: '/register',
+          element: <Register/>
+        },
+        {
+          path: '/login',
+          element: <Login/>
+        },
+        {
+          path: '/workout',
+          element: <Workout/>
+        },
+        {
+          path: '/plans',
+          element: <Plans/>
+        },
+        {
+          path: '/customers',
+          element: <Customers/>
+        },
+        {
+          path: '/library',
+          element: <h1>library</h1>
+        },
+        {
+          path: '/dashboard',
+          element: <Dashboard/>
+        }
+      ]
+    },
+])
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NavBar />
+      {/* <NavBar /> */}
+      <RouterProvider router={router}/>
       {/* <Typography variant="h1">Welcome to My App</Typography>
          <Button variant="contained" color="primary">Click Me</Button>   */}
     </ThemeProvider>
@@ -16,3 +59,5 @@ function App() {
 }
 
 export default App;
+
+
