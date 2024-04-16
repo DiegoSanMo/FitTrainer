2910dc4e45209e8938fc2b306ed8bf6fb5291335
Diego Mosan <diegomosan@Diegos-MacBook-Pro.local>
2024-04-15 23:51:56 -0600

    Added new library for languages

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
 import { Button, Typography } from "@mui/material";
 import { NavBar } from "./components/NavBar/NavBar.tsx";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <NavBar />
-      {/* <Typography variant="h1">Welcome to My App</Typography>
-         <Button variant="contained" color="primary">Click Me</Button>   */}
-    </ThemeProvider>
+    <Suspense fallback="loading">
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        {/* <Typography variant="h1">Welcome to My App</Typography>
+            <Button variant="contained" color="primary">Click Me</Button>   */}
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
