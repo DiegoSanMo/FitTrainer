07f64a4f98839da4169250d1e35014b5e8404b9f
Guerra-San <leoficial84@gmail.com>
2024-05-07 23:48:30 -0600

    Styled and fetch from the API to create a profile's form. I used the metod slice to divide data  in three different Boxes

--- src/pages/Customers/EditCustomer.tsx
+++ src/pages/Customers/EditCustomer.tsx
@@ -6,14 +6,11 @@ import {
   ImageList,
   ImageListItem,
   Paper,
-  Input,
-  InputLabel,
-  FormControl,
 } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
+function srcset(image: string, size: number, rows = 5, cols = 5) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
     srcSet: `${image}?w=${size * cols}&h=${
@@ -26,15 +23,56 @@ export default function EditCustomer() {
   const { t } = useTranslation();
   const location = useLocation();
   const { itemResponses, respondentEmail } = location.state;
+  // function Slice(a, b) {
+  //   itemResponses.slice(a, b);
+  // }
+  const anSlice = itemResponses.slice(0, 2);
+  const ans = anSlice.map((ans) => (
+    <div>
+      <TextField
+        sx={{ width: "30vw", m: 1 }}
+        id=""
+        label={ans.question}
+        value={ans.answer}
+        disable
+      />
+    </div>
+  ));
+
+  const reSlice = itemResponses.slice(3, 9);
+  const res = reSlice.map((res) => (
+    <div>
+      <TextField
+        sx={{ width: "100%" }}
+        id="standard-basic"
+        label={res.question}
+        value={res.answer}
+      />
+    </div>
+  ));
+  const TreSlice = itemResponses.slice(9, 15);
+  const Tres = TreSlice.map((Tres) => (
+    <div>
+      <TextField
+        id="outlined-multiline-static"
+        sx={{ width: "100%" }}
+        label={Tres.question}
+        defaultValue={Tres.answer}
+        disable
+        multiline
+      />
+    </div>
+  ));
+
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={9}>
           <ImageList
             sx={{ width: "30vw", height: "50vh" }}
             variant="quilted"
             cols={1}
-            rowHeight={121}
+            rowHeight={127}
           >
             {itemData.map((item) => (
               <ImageListItem
@@ -50,45 +88,74 @@ export default function EditCustomer() {
               </ImageListItem>
             ))}
           </ImageList>
+          {ans}
           <TextField
-            sx={{ width: "77%", top: "0%" }}
             id=""
-            label="Usuario"
-            value={itemResponses[0].answer}
+            sx={{ width: "30vw", m: 1 }}
+            label="E-mail"
+            value={respondentEmail}
+            disable
           />
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h3" color="primary">
+        <Grid item xs={12} sm={3}>
+          <Typography
+            variant="h3"
+            color="primary"
+            sx={{
+              position: "absolute",
+              left: "50%",
+              margin: "1%",
+            }}
+          >
             {t("edit-customer.account-detail")}
           </Typography>
+          <br />
+          <br />
+          <br />
 
-          <Paper elevation={3} sx={{ margin: "0%" }}>
+          <Paper
+            elevation={3}
+            sx={{
+              width: "20%",
+              margin: "1%",
+              position: "absolute",
+              left: "50%",
+            }}
+          >
             <Box
               component="form"
               sx={{
-                "& .MuiTextField-root": { m: 1, width: "98%" },
+                "& .MuiTextField-root": { m: 1, width: "95%" },
               }}
               noValidate
               autoComplete="off"
             >
               <div>
-                <TextField
-                  sx={{ width: "100%" }}
-                  id="standard-basic"
-                  label="Correo"
-                  value={respondentEmail}
-                />
+                {res}
                 <br />
-                <FormControl
-                  variant="standard"
-                  sx={{ width: "97%", margin: "1em" }}
-                >
-                  <InputLabel htmlFor="component-simple">Name</InputLabel>
-                  <Input
-                    id="component-simple"
-                    defaultValue={itemResponses[0].answer}
-                  />
-                </FormControl>
+              </div>
+            </Box>
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              width: "27vw",
+              margin: "1%",
+              position: "absolute",
+              left: "71%",
+            }}
+          >
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "95%" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div>
+                {Tres}
+                <br />
               </div>
             </Box>
           </Paper>
@@ -100,7 +167,7 @@ export default function EditCustomer() {
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    img: "https:images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "",
     rows: 3,
     cols: 3,
