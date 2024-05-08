7bd6cfccc70b385be31486f6983d17b8572a94fc
Guerra-San <leoficial84@gmail.com>
2024-05-08 00:00:26 -0600

    EditCustomer has been erased. Changes in AddCustomer, styled and API´s fetch to create a Profile´s form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,56 +1,68 @@
-import { createBrowserRouter } from "react-router-dom"
-import { HomeLayout, ErrorP, Landing, Register, Login, Workout, Plans, Customers, Dashboard, GoogleForms, EditCustomer } from "../pages";
+import { createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  ErrorP,
+  Landing,
+  Register,
+  Login,
+  Workout,
+  Plans,
+  Customers,
+  Dashboard,
+  GoogleForms,
+  AddCustomer,
+} from "../pages";
 
 const ROUTER = createBrowserRouter([
-    {
-      path: '/',
-      element: <HomeLayout/>,
-      errorElement: <ErrorP/>,
-      children: [
-        {
-          index: true,
-          element: <Landing/>
-        },
-        {
-          path: '/register',
-          element: <Register/>
-        },
-        {
-          path: '/login',
-          element: <Login/>
-        },
-        {
-          path: '/workout',
-          element: <Workout/>
-        },
-        {
-          path: '/plans',
-          element: <Plans/>
-        },
-        {
-          path: '/customers',
-          element: <Customers/>
-        },
-        {
-          path: '/library',
-          element: <h1>library</h1>
-        },
-        {
-          path: '/dashboard',
-          element: <Dashboard/>,
-          children: [
-            {
-              index: true,
-              element: <GoogleForms/>
-            },
-            {
-              path: 'customer',
-              element:<EditCustomer/>
-            },
-          ]
-        }
-      ]
-    },
-  ])
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <ErrorP />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/workout",
+        element: <Workout />,
+      },
+      {
+        path: "/plans",
+        element: <Plans />,
+      },
+      {
+        path: "/customers",
+        element: <Customers />,
+      },
+      {
+        path: "/library",
+        element: <h1>library</h1>,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+        children: [
+          {
+            index: true,
+            element: <GoogleForms />,
+          },
+          {
+            path: "customer",
+            element: <AddCustomer />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-  export default ROUTER;
+export default ROUTER;
