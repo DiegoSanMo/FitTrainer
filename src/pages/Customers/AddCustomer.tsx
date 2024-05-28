3b5fe8928d0df66f5eb1c2c61ab32065ff7d6f28
Diego Mosan <diegomosan@Diegos-MBP.bbrouter>
2024-05-27 22:50:23 -0600

    Refactor screen

--- src/pages/Customers/AddCustomer.tsx
+++ src/pages/Customers/AddCustomer.tsx
@@ -2,13 +2,13 @@ import {
   Box,
   TextField,
   Grid,
-  Typography,
   ImageList,
   ImageListItem,
   Paper,
+  Tooltip,
 } from "@mui/material";
 import { useLocation } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function srcset(image: string, size: number, rows = 5, cols = 5) {
   return {
@@ -19,148 +19,120 @@ function srcset(image: string, size: number, rows = 5, cols = 5) {
   };
 }
 
+export interface QuizI {
+  question: string;
+  answer: string;
+}
+
 export default function AddCustomer() {
-  const { t } = useTranslation();
+  
   const location = useLocation();
   const { itemResponses, respondentEmail } = location.state;
-  // function Slice(a, b) {
-  //   itemResponses.slice(a, b);
-  // }
-  const anSlice = itemResponses.slice(0, 2);
-  const ans = anSlice.map((ans) => (
-    <div>
-      <TextField
-        sx={{ width: "30vw", m: 1 }}
-        id=""
-        label={ans.question}
-        value={ans.answer}
-        disable
-      />
-    </div>
-  ));
+  const [userData] = useState<QuizI[]>(itemResponses);
 
-  const reSlice = itemResponses.slice(3, 9);
-  const res = reSlice.map((res) => (
-    <div>
-      <TextField
-        sx={{ width: "100%" }}
-        id="standard-basic"
-        label={res.question}
-        value={res.answer}
-      />
-    </div>
-  ));
-  const TreSlice = itemResponses.slice(9, 15);
-  const Tres = TreSlice.map((Tres) => (
-    <div>
-      <TextField
-        id="outlined-multiline-static"
-        sx={{ width: "100%" }}
-        label={Tres.question}
-        defaultValue={Tres.answer}
-        disable
-        multiline
-      />
-    </div>
-  ));
+  const renderUserContactInfo = () => {
+    const userDataInfo = userData.slice(0, 4);
+    return (
+      <Grid sm={12} spacing={3}>
+        {userDataInfo.map((data) => {
+          return (
+            <TextField
+              sx={{ width: "100%", marginTop: "15px" }}
+              id=""
+              label={data.question}
+              value={data.answer}
+            />
+          );
+        })}
+      </Grid>
+    );
+  };
+
+  const renderShortUserQuizAnswers = () => {
+    const userQuiz = userData.slice(4, 12);
+    return userQuiz.map((data) => {
+      return (
+        <Grid item sm={4} spacing={3}>
+          <Tooltip
+            title={data.question}
+            placement="top-start"
+            arrow
+            sx={{ background: "white", color: "black" }}
+          >
+            <TextField
+              sx={{ width: "100%", marginTop: "15px" }}
+              id=""
+              label={data.question}
+              value={data.answer}
+            />
+          </Tooltip>
+        </Grid>
+      );
+    });
+  };
+
+  const renderLongUserQuizAnswers = () => {
+    const userQuiz = userData.slice(12, userData.length);
+    return userQuiz.map((data) => {
+      return (
+        <Grid item sm={12} spacing={3}>
+          <Tooltip
+            title={data.question}
+            placement="top-start"
+            arrow
+            sx={{ background: "white", color: "black" }}
+          >
+            <TextField
+              sx={{ width: "100%", height: '40px', marginTop: "15px" }}
+              id=""
+              label={data.question}
+              value={data.answer}
+            />
+          </Tooltip>
+        </Grid>
+      );
+    });
+  }
 
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={9}>
-          <ImageList
-            sx={{ width: "30vw", height: "50vh" }}
-            variant="quilted"
-            cols={1}
-            rowHeight={127}
-          >
-            {itemData.map((item) => (
-              <ImageListItem
-                key={item.img}
-                cols={item.cols || 1}
-                rows={item.rows || 1}
-              >
-                <img
-                  {...srcset(item.img, 121, item.rows, item.cols)}
-                  alt={item.title}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          {ans}
-          <TextField
-            id=""
-            sx={{ width: "30vw", m: 1 }}
-            label="E-mail"
-            value={respondentEmail}
-            disable
-          />
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Typography
-            variant="h3"
-            color="primary"
-            sx={{
-              position: "absolute",
-              left: "50%",
-              margin: "1%",
-            }}
-          >
-            {t("edit-customer.account-detail")}
-          </Typography>
-          <br />
-          <br />
-          <br />
-
-          <Paper
-            elevation={3}
-            sx={{
-              width: "20%",
-              margin: "1%",
-              position: "absolute",
-              left: "50%",
-            }}
-          >
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "95%" },
-              }}
-              noValidate
-              autoComplete="off"
+      <Paper elevation={3} sx={{ padding: 2, marginBottom: "30px" }}>
+        <Grid container direction={"row"} spacing={3}>
+          <Grid item sm={6}>
+            <ImageList
+              sx={{ width: "100%", height: "auto" }}
+              variant="quilted"
+              cols={1}
+              rowHeight={127}
             >
-              <div>
-                {res}
-                <br />
-              </div>
-            </Box>
-          </Paper>
-          <Paper
-            elevation={3}
-            sx={{
-              width: "27vw",
-              margin: "1%",
-              position: "absolute",
-              left: "71%",
-            }}
-          >
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "95%" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <div>
-                {Tres}
-                <br />
-              </div>
-            </Box>
-          </Paper>
+              {itemData.map((item) => (
+                <ImageListItem
+                  key={item.img}
+                  cols={item.cols || 1}
+                  rows={item.rows || 1}
+                >
+                  <img
+                    {...srcset(item.img, 121, item.rows, item.cols)}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+          <Grid item sm={6}>
+            {renderUserContactInfo()}
+          </Grid>
         </Grid>
-      </Grid>
+      </Paper>
+      <Paper elevation={3} sx={{ padding: 2 }} >
+        <Grid sm={12} container direction={"row"} spacing={2}>
+          {renderShortUserQuizAnswers()}
+        </Grid>
+        <Grid sm={12} container direction={"row"} spacing={2} sx={{marginBottom: "20px"}}>
+          {renderLongUserQuizAnswers()}
+        </Grid>
+      </Paper>
     </Box>
   );
 }
